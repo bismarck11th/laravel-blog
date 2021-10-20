1a35ef7a314f405d0789295cbd1ab8d72ae9a2d9
kazuki827 <xm.kazuki827@gmail.com>
2021-10-20 16:22:51 +0900

    make routing

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,32 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/*
+[ ルーティング書き方 ]
+Route::get('blogs', 'BlogController@showList');
+
+Route          -> Laravelの既存機能のRouteファサードを使う
+get            -> HTTPメソッドを指定
+blogs          -> 任意のURLを指定
+BlogController -> 作ったControllerを指定
+@showList      -> Controllerのメソッド名を指定
+
+[ ファサードとは? ]
+参考 https://qiita.com/minato-naka/items/095f2a1beec1d09f423e
+
+ファサードとは、
+クラスをインスタンス化しなくてもstaticメソッドのように
+メソッドを実行できるようにしてくれる機能のこと。
+
+通常
+$classA = new ClassA();
+$classA->methodA();
+
+ファサード利用
+FacadeClassA::methodA();
+このインスタンス化している１行の手間をなくせるっていうだけっちゃだけですかね。
+*/
+
+// ブログ一覧画面
+Route::get('', 'BlogController@showList')->name('blogs');
+
